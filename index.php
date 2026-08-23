7710fc64b9070b17768e21f262bc96e43f72f44d
marcomandolino <40366911+marcomandolino@users.noreply.github.com>
2026-08-23 13:29:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>INEDIA PROJECT: LSWF (Life-Style Without Food) ovvero LO STILE DI VITA SENZA CIBO - LSFW - HOME</title>
+<meta name="description" content="LSWF - Sito dedicato al più completo libro in italiano sull'alimentazione pranica, sul breatharianismo e sul nutrirsi di Luce">
+<meta name='keywords' content='breatharianismo, breathariano, breathariani, inedia totale,  non-eating, alimentazione di Luce,  nutrimento pranico, nutrirsi di Luce, nutrimento solare, sungazing, relativi metodi pratici per raggiungere questi stadi, digiuno terapeutico, autoguarigione, disintossicazione ed eliminazione scorie, reale funzionamento del corpo umano, esseri non materiali, Jasmuheen, Arnold Ehret, dieta senza muco, il sistema di guarigione della dieta senza muco, il digiuno può salvarti la vita, riposo fosiologico, liquidarismo, dieta liquirariana, liquidariano, Shelton, fruttarianismo, fruttarismo, frutta, verdure, crudismo, fruttariano, fruttariani, crudista, crudisti, Amore universale, Dio, Krishna, Ayurveda, riprogrammazione cellulare, riprommazione e PNL, PNL, riprogrammazione dell'istinto, ringiovanimento fisico, ringiovanimento cellulare, digiuno e ringiovanimento, influenza del condizionamento sociale, immortalità, immortale, eat free, liberarasi dalle dipendenze, dipendenza dal cibo, fame emotiva, fenomeni fisici del misticismo, santo, santi, guru' />
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+<link rel='stylesheet' href='http://cdn1.editmysite.com/editor/images/common/common-v2.css?buildTime=1344299624' type='text/css' />
+<link rel='stylesheet' type='text/css' href='files/main_style.css?1344425947' title='wsite-theme-css' />
+<style type='text/css'>
+#wsite-content div.paragraph, #wsite-content p, #wsite-content .product-description, .blog-sidebar div.paragraph, .blog-sidebar p, .wsite-form-field label, .wsite-form-field label {}
+#wsite-content h2, #wsite-content .product-title, .blog-sidebar h2{}
+#wsite-title{
+	font-size: 26px;
+}
+</style>
+<script type='text/javascript'><!--
+var STATIC_BASE = 'http://cdn1.editmysite.com/';
+var STYLE_PREFIX = 'wsite';
+//-->
+</script>
+<script type='text/javascript' src='http://cdn1.editmysite.com/libraries/prototype/1.7-custom/prototype.min.js'></script>
+<script type='text/javascript' src='http://cdn1.editmysite.com/libraries/scriptaculous/1.9.0-custom/effects.min.js'></script>
+<script type='text/javascript' src='http://cdn1.editmysite.com/editor/images/common/utilities.js?1344299624'></script>
+<script type='text/javascript' src='http://cdn1.editmysite.com/editor/images/common/lightbox202.js?1344299624'></script>
+<script type='text/javascript' src='http://cdn1.editmysite.com/languages/it/libraries/flyout_menus.js?1344299624'></script>
+<script type='text/javascript'><!--
+var IS_ARCHIVE=1;
+function initFlyouts(){initPublishedFlyoutMenus([{"id":"968965016925691939","title":"Home","url":"index.html"},{"id":"344249334943857013","title":"Prefazione","url":"prefazione.html"},{"id":"744372037631863176","title":"Joachim e la sua esperienza","url":"joachim-e-la-sua-esperienza.html"},{"id":"636033730917337539","title":"Indice e argomenti del libro","url":"indice-e-argomenti-del-libro.html"},{"id":"841528442245803662","title":"Estratto PDF","url":"estratto-pdf.html"},{"id":"294305203727629387","title":"Acuista libro","url":"acuista-libro.html"},{"id":"757214418817897918","title":"Contatti","url":"contatti.html"}],'968965016925691939',"<li class='wsite-nav-more'><a href='#'>altro...<\/a><\/li>",'active',false)}
+if (Prototype.Browser.IE) window.onload=initFlyouts; else document.observe('dom:loaded', initFlyouts);
+//-->
+</script>
+<style type="text/css">
+@import url("files/main_style.css");
+@import url("files/theme/main_style.css");
+.wsite-theme-light.tall-header-page.wsite-page-index #container-wrap1 #container-wrap2 #container-wrap3 #container #content #wsite-content table tr td ul li {
+	font-size: 14px;
+}
+.wsite-theme-light.tall-header-page.wsite-page-index #container-wrap1 #container-wrap2 #container-wrap3 #container #content #wsite-content table tr td ul li {
+	font-size: 16px;
+}
+#container-wrap1 #container-wrap2 #container-wrap3 #container #content #wsite-content table tr td ul li font {
+	font-size: 18px;
+}
+.wsite-theme-light.tall-header-page.wsite-page-index #container-wrap1 #container-wrap2 #container-wrap3 #container #content #wsite-content table tr td ul li font {
+	font-size: 20px;
+}
+.wsite-theme-light.tall-header-page.wsite-page-index #container-wrap1 #container-wrap2 #container-wrap3 #container #content #wsite-content table tr td ul li font {
+	font-size: 21px;
+}
+.wsite-theme-light.tall-header-page.wsite-page-index #container-wrap1 #container-wrap2 #container-wrap3 #container #content #wsite-content table tr td ul li font {
+	font-size: 23px;
+}
+.wsite-theme-light.tall-header-page.wsite-page-index #container-wrap1 #container-wrap2 #container-wrap3 #container #content #wsite-content table tr td ul li font {
+	font-size: 25px;
+}
+.wsite-theme-light.tall-header-page.wsite-page-index #container-wrap1 #container-wrap2 #container-wrap3 #container #content #wsite-content table tr td {
+	text-align: justify;
+}
+.wsite-theme-light.tall-header-page.wsite-page-index #container-wrap1 #container-wrap2 #container-wrap3 #container #content #wsite-content table {
+	font-style: italic;
+}
+.wsite-theme-light.tall-header-page.wsite-page-index #container-wrap1 #container-wrap2 #container-wrap3 #container #content #wsite-content table {
+	font-weight: bold;
+}
+.wsite-theme-light.tall-header-page.wsite-page-index #container-wrap1 #container-wrap2 #container-wrap3 #container #content #wsite-content h2 font {
+	font-size: 27px;
+	font-weight: bold;
+	text-align: center;
+}
+</style>
+</head>
+<body class='wsite-theme-light tall-header-page wsite-page-index'>
+<div id="container-wrap1">
+  <div id="container-wrap2">
+    <div id="container-wrap3">
+	<div id="container">
+				<p><img src="files/theme/container_top2.gif" alt="" width="927" height="30"></p>
+				<div id="banner">
+				  <div class="wsite-header"></div>
+	    </div>
+				<div id="navigation">
+					<div id="navigation-detail">
+						<ul><li id='active'><a href='index.php'>Home</a></li><li id='pg344249334943857013'><a href='prefazione.html'>Prefazione</a></li><li id='pg744372037631863176'><a href='joachim-e-la-sua-esperienza.html'>Joachim e la sua esperienza</a></li><li id='pg636033730917337539'><a href='indice-e-argomenti-del-libro.html'>Indice e argomenti del libro</a></li><li id='pg841528442245803662'><a href='estratto-pdf.html'>Estratto PDF</a></li>
+						<li id='pg294305203727629387'><a href='acquista-libro.html'>Acquista libro</a></li><li id='pg757214418817897918'><a href='contatti.html'>Contatti</a></li></ul>
+						<div class="clear"></div>
+					</div>
+				</div>
+				<div id="content">
+					<div id='wsite-content' class='wsite-not-footer'>
+<h2 style="text-align:left;"><font size="5">LSWF (Life Style Without Food) ovvero lo STILE DI VITA SENZA CIBO</font></h2>
+<h2 style="text-align:left;"><font size="5">  IL LIBRO IN ITALIANO</font></h2>
+<h2 style="text-align:left; font-size: 22px;"><span></span>Si tratta  della più completa opera-manuale sul fenomeno del breatharianismo/inedia totale.</h2>
+<div style="display: block; font-size: 90%; margin-top: -10px; margin-bottom: 10px; text-align: center;"></div>
+<div class="paragraph" style="text-align:justify;display:block;"><font size="3">L&rsquo;esperienza diretta di Joachim M Werdin raccontata e descritta in dettaglio su:</font></div>
+<p style="text-align:left;">&nbsp;</p>
+<table width="924" height="440" border="1">
+  <tr>
+    <td width="294" height="434"><a><img src="uploads/1/3/1/9/13193250/4997941.jpg" alt="Immagine" width="276" height="407" class="galleryImageBorder" style="margin-top: 5px; margin-bottom: 10px; margin-left: 0px; margin-right: 10px; border-width:1px;padding:3px;" /></a></td>
+    <td width="614"><ul>
+      <li><font size="3"><strong>►</strong>breatharianismo, inedia totale</font></li>
+      <li><font size="3"> <strong>►</strong>non-eating, alimentazione di luce</font></li>
+      <li><font size="3"><strong>►</strong>nutrimento pranico e solare</font></li>
+      <li><font size="3"><strong>►</strong>sungazing</font></li>
+      <li><font size="3"><strong>►</strong>relativi metodi pratici per raggiungere questi stadi</font></li>
+      <li><font size="3"><strong>►</strong>digiuno terapeutico e autoguarigione</font></li>
+      <li><font size="3"><strong>►</strong>disintossicazione ed eliminazione scorie</font></li>
+      <li><font size="3"><strong>►</strong>reale funzionamento del corpo umano</font></li>
+      <li><font size="3"><strong>►</strong>ringiovanimento fisico</font></li>
+      <li><font size="3"><strong>►</strong>influenza del condizionamento sociale</font></li>
+      <li><font size="3"><strong>►</strong>riprogrammazione dell&rsquo;istinto</font></li>
+      <li><font size="3"><strong>►</strong>sviluppo personale e spirituale</font></li>
+      <li><font size="3"><strong>►</strong>espansione della propria sfera di Coscienza</font></li>
+      <li><font size="3"><strong>►</strong>esseri non-materiali</font></li>
+    </ul></td>
+  </tr>
+</table>
+<h2 style="text-align:left;">&nbsp;</h2>
+<h2 style="text-align:left;">Scheda e specifiche tecniche:<br />
+</h2>
+<table cellspacing="0" cellpadding="0">
+  <tr>
+    <td><img alt="digiuno razionale" src="uploads/1/3/1/9/13193250/438047_orig.jpg" height="250" width="174"></td>
+    <td>&nbsp;</td>
+    <td><table summary="" border="1" cellspacing="0">
+      <tbody>
+        <tr>
+          <td height="20"> Titolo </td>
+          <td>"<span style="text-align:left;">LSWF (Life Style Without Food) ovvero lo STILE DI VITA SENZA CIBO</span>" </td>
+        </tr>
+        <tr>
+          <td> Autore </td>
+          <td>Joachim M Werdin</td>
+        </tr>
+        <tr>
+          <td>Titolo originale polacco</td>
+          <td>&quot;Styl zycia Bez Jedzenia&quot;</td>
+        </tr>
+        <tr>
+          <td> Titolo originale inglese</td>
+          <td>"Life Stile Without Food" </td>
+        </tr>
+        <tr>
+          <td> Traduzione, note critiche e commenti</td>
+          <td> Marco Giai-Levra</td>
+        </tr>
+        <tr>
+          <td> Formato, dimensioni e pagine </td>
+          <td> A5, cm. 148 x 210, pag. 216</td>
+        </tr>
+        <tr>
+          <td> Rilegatura </td>
+          <td> brossura termica</td>
+        </tr>
+        <tr>
+          <td colspan="2">Copertina morbida </td>
+          <td></td>
+        </tr>
+        <tr>
+          <td> Editore-marchio</td>
+          <td> Marco Giai-Levra</td>
+        </tr>
+        <tr>
+          <td> Data di pubblicazione </td>
+          <td> Settembre 2012</td>
+        </tr>
+        <tr>
+          <td> Edizione </td>
+          <td> prima </td>
+        </tr>
+        <tr>
+          <td colspan="2"> ISBN <span style="text-align:left;">978-88-907655-0-6</span></td>
+        </tr>
+        <tr>
+          <td> Prezzo </td>
+          <td> € 18 </td>
+        </tr>
+      </tbody>
+    </table></td>
+  </tr>
+</table>
+<p style="text-align:left;">&nbsp;</p>
+<p style="text-align:left;">
+
+<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="4533PM7GMDGCW">
+<table>
+<tr><td><input type="hidden" name="on0" value="LSWF - Opzioni e sconti">LSWF - Opzioni e sconti</td></tr><tr><td><select name="os0">
+	<option value="1 libro, tot.">1 libro, tot. €18,00 EUR</option>
+	<option value="2 libri, sconto 3,00 euro, tot.">2 libri, sconto 3,00 euro, tot. €33,00 EUR</option>
+	<option value="3 libri, sconto 9,00 euro, tot.">3 libri, sconto 9,00 euro, tot. €45,00 EUR</option>
+</select> </td></tr>
+</table>
+<input type="hidden" name="currency_code" value="EUR">
+<input type="image" src="https://www.paypalobjects.com/it_IT/IT/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - Il metodo rapido, affidabile e innovativo per pagare e farsi pagare.">
+<img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+
+
+
+  <img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">Il costo di spedizione è pari a € 1,80 e rimarrà invariato anche nel caso si decida di acquistare più copie del libro.</p>
+  <p style="text-align:left;">____________________________________________________________________________________________________________________________________</p>
+<p style="text-align:left;">Note di copyrights<font size="3"><strong></strong></font><span class='imgPusher' style='float:left;height:0px'></span><span style='position:relative;float:left;z-index:10;;clear:left;margin-top:5px;*margin-top:10px'></span><strong><font size="3">:</font></strong></p>
+<p style="text-align:left;"><strong><font size="3">C</font></strong><font size="3"><strong>opyright &copy; 2012 Marco Giai-Levra. </strong></font><br />   Note di copyright della presente elaborazione di LSWF di Joachim M Werdin:<br />   <br />    
+Prima edizione, anno 2012, traduzione e riadattamento dall&rsquo;inglese    all&rsquo;italiano, redazione, note critiche, note di traduzione, note di    redazione, revisione, editing, impaginazione, grafica, copertina e    layout a cura di Marco Giai-Levra. Propriet&agrave; letteraria e tutti gli    altri diritti riservati, copyright &copy; 2012 Marco Giai-Levra. <br />   <br />  Il   contenuto e l&rsquo;opera tradotti in italiano presentati su queste  pagine   sono soggetti alle normative sul diritto d&rsquo;autore. Pertanto la    riproduzione, l&rsquo;elaborazione, la diffusione e tutti gli altri utilizzi    esclusi dal copyright richiedono l&rsquo;approvazione scritta del  rispettivo   autore o creatore dell&rsquo;elaborazione. L&rsquo;autore della  presente   elaborazione &egrave; Marco Giai-Levra. La duplicazione di queste  pagine non &egrave;   permessa in nessuna forma.<br />
+  <span></span> ____________________________________________________________________________________________________________________________________<br /> siti web: </p>
+<p style="text-align:left;"><a href="http://www.inediatotale.com" target="_blank">http://www.inediatotale.com<br>
+</a><a href="http://inedia.tk" target="_blank">http://inedia.tk/</a><br /> 
+  <a title="" style="" href="http://www.inedia.net84.net/" target="_blank">http://www.inedia.net84.net/</a><br /> 
+  <a title="" style="" href="http://www.facebook.com/groups/inedia/" target="_blank">http://www.facebook.com/groups/inedia/</a><br /> 
+  <a title="" style="" href="http://breatharian.info/" target="_blank">http://breatharian.info/</a><br /> 
+  <a title="" style="" href="http://inedia.info/" target="_blank">http://inedia.info/</a></p>
+<p style="text-align:left;">____________________________________________________________________________________________________________________________________</p>
+<table width="926" height="68" border="1">
+  <tr>
+    <td width="916">ATTENZIONE: il contenuto di questo libro è da considerarsi puramente a carattere informativo e non      costituisce un consulto medico, né sostituisce in nessun caso una valutazione o una      prescrizione medica. Il lettore che utilizzerà tali informazioni in modo incosciente,      lo farà unicamente ed esclusivamente sotto la sua responsabilità. Pertanto l'editore,      l'autore e il traduttore dell'opera declinano ogni responsabilità conseguente ad un      uso sconsiderato delle informazioni contenutevi.</td>
+  </tr>
+</table>
+<p style="text-align:left;">&nbsp;</p>
+<hr style="clear:both;visibility:hidden;width:100%;"></hr></div>
+
+					<div class="clear"></div>
+				</div>
+	  </div>
+	</div>
+  </div>
+</div>
+<div id="footer"></div>
+
+</body>
+</html>
